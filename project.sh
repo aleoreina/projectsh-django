7704b9884356d48032160d9a83d1e646b13f33f9
Angel <aleoreina@gmail.com>
2019-11-08 11:47:59 -0400

    localhost, Run Server, Create superadmin, Fixed do migrations all models

--- project.sh
+++ project.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# github: projectsh-django
+# autor: Angel Reina (@aleoreina) - Venezuela
 
 function template_header {  
 clear	
@@ -16,9 +17,12 @@ function template_main_menu {
     echo -e "--------------------------------------------------------"
 	echo -e " \e[101m Manage.py functions \e[49m "
     echo -e "--------------------------------------------------------"
-    echo "1) Run Server :: 0.0.0.0:8080"
-    echo "2) Do make migrations (structure) for all models.py to Database "
-    echo "3) Do migrations for all models.py to Database "
+    echo "1) Run Server :: 127.0.0.1:8080"
+    echo "2) Run Server :: 0.0.0.0:8080"
+    echo "3) Run Shell for project"
+    echo "4) Create SuperAdmin User"
+    echo "5) Do make migrations (structure) for all models.py to Database "
+    echo "6) Do migrations for all models.py to Database "
     echo -e "--------------------------------------------------------"
 
 
@@ -34,7 +38,9 @@ function template_main_menu {
 
     echo "X) Exit"
 }
-
+function msg_backing_to_main_menu {
+    echo "Redirecting to main menu in 2 seconds..."
+}
 function install_requirements_txt {
     template_header
     echo "Installing req.txt"
@@ -47,11 +53,26 @@ function install_packages_json_via_yarn {
     yarn
     sleep 2
 }
+function run_shell {
+    template_header
+    echo "Running shell for Project "
+    python ./manage.py shell
+    msg_backing_to_main_menu
+    sleep 2
+}
+function run_server_for_127_0_0_1_port_8080 {
+    template_header
+    echo "Running Server for 127.0.0.1:8080 for Django.. "
+    python ./manage.py runserver 127.0.0.1:8080
+    msg_backing_to_main_menu
+
+    sleep 2
+}
 function run_server_for_all_ips_port_8080 {
     template_header
     echo "Running Server for 0.0.0.0:8080 for Django.. "
     python ./manage.py runserver 0.0.0.0:8080
-    echo "Redirecting to main menu in 2 seconds..."
+    msg_backing_to_main_menu
 
     sleep 2
 }
@@ -59,14 +80,21 @@ function run_make_migrations {
     template_header
     echo "Creating structured migrations  "
     python ./manage.py makemigrations
-    echo "Redirecting to main menu in 2 seconds..."
+    msg_backing_to_main_menu
     sleep 2
 }
 function run_migrate {
     template_header
     echo "Doing migrations to DB "
-    python ./manage.py makemigrations
-    echo "Redirecting to main menu in 2 seconds..."
+    python ./manage.py migrate
+    msg_backing_to_main_menu
+    sleep 2
+}
+function create_superadmin {
+    template_header
+    echo "Create a superadmin user "
+    python ./manage.py createsuperuser
+    msg_backing_to_main_menu
     sleep 2
 }
 # Purpose - Get input via the keyboard and make a decision using case..esac 
@@ -75,9 +103,12 @@ function main_menu(){
 	local c
 	read -p "Enter the code > " c
 	case $c in
-    	1)	run_server_for_all_ips_port_8080 ;;
-        2)	run_make_migrations ;;
-        3)  run_migrate ;;
+        1)  run_server_for_127_0_0_1_port_8080 ;; 
+    	2)	run_server_for_all_ips_port_8080 ;;
+        3)  run_shell ;;
+        4)  create_superadmin ;;
+        5)	run_make_migrations ;;
+        6)  run_migrate ;;
 		R1)	install_requirements_txt ;;
         R2)	install_packages_json_via_yarn ;;
 		X)	echo "Bye!"; exit 0 ;;
@@ -87,7 +118,7 @@ function main_menu(){
 	esac
 }
 
-# ignore CTRL+C, CTRL+Z and quit singles using the trap
+# ignore CTRL+C, CTRL+Z 
 #trap '' SIGINT SIGQUIT SIGTSTP
 
 while true
